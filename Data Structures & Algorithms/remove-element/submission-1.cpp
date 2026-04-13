f13f1a18e2b4329957e4d93e8c33cff29b478f15
bytecaleb <calebriley2404@gmail.com>
2026-04-13 21:10:54 +1000

    Add: remove-element - submission-1

---
+++ Data Structures & Algorithms/remove-element/submission-1.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) 
+    {
+        int k = 0;
+
+        for (int n : nums)
+        {
+            if (n != val)
+            {
+                nums[k] = n;
+                k++;
+            }
+        }
+        return k;
+    }
+};
